7957a90826ce5f2268b3c9b636581e1978ba097f
Rodney Peck <ropeck@paypal.com>
2013-12-14 09:17:38 -0800

    allow switching devstack version

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ apt-get install git -y
 test -d devstack || \
 	sudo -u vagrant git clone git://github.com/openstack-dev/devstack.git 
 cd devstack
-sudo -u vagrant git checkout stable/havana
+sudo -u vagrant git checkout stable/grizzly
 cat <<EOF > localrc
 ADMIN_PASSWORD=password
 MYSQL_PASSWORD=password
